47509670c4501575d2385fb48e14314c9920a925
Maxmiller Nunes <maxmillernunes11@gmail.com>
2025-11-29 18:09:31 -0300

    feat: :memo: add prisma and config database

---
+++ prisma/migrations/20251129210804_create_tables_users_and_otps/migration.sql
@@ -0,0 +1,28 @@
+-- CreateTable
+CREATE TABLE "users" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3),
+
+    CONSTRAINT "users_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "otps" (
+    "id" TEXT NOT NULL,
+    "code" TEXT NOT NULL,
+    "user_id" TEXT NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "expires_at" TIMESTAMP(3) NOT NULL,
+    "used" BOOLEAN NOT NULL DEFAULT false,
+
+    CONSTRAINT "otps_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_email_key" ON "users"("email");
+
+-- AddForeignKey
+ALTER TABLE "otps" ADD CONSTRAINT "otps_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
--- src/env/index.ts
+++ src/env/index.ts
@@ -6,8 +6,8 @@ const envSchema = z.object({
     .enum(['development', 'production', 'test'])
     .default('development'),
   PORT: z.coerce.number().default(3333),
-  // DATABASE_URL: z.url(),
-  // JWT_SECRET: z.string(),
+  DATABASE_URL: z.url().startsWith('postgres://'),
+  JWT_SECRET: z.string(),
 })
 
 const _env = envSchema.safeParse(process.env)
