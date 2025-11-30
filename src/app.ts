6f9c502364946706aff58c2053f80548b863c84b
Maxmiller Nunes <maxmillernunes11@gmail.com>
2025-11-29 22:30:56 -0300

    feat: :sparkles: add error handler

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
-import express, { urlencoded } from 'express'
-import helmet from 'helmet'
+import express, { urlencoded, Request, Response, NextFunction } from 'express'
 import cors from 'cors'
+import helmet from 'helmet'
+import z, { ZodError } from 'zod'
 import { router } from './infra/routes'
 
 const app = express()
@@ -11,5 +12,25 @@ app.use(urlencoded({ extended: true }))
 app.use(express.json())
 
 app.use(router)
+
+// Global error handling middleware
+app.use(
+  (error: Error, request: Request, response: Response, next: NextFunction) => {
+    if (error instanceof ZodError) {
+      return response.status(400).send({
+        message: 'Validation error',
+        error: z.treeifyError(error),
+      })
+    }
+
+    // Handle other types of errors
+
+    // Error fallback
+    if (error) {
+      return response.status(500).json({ message: error.message })
+    }
+    next()
+  }
+)
 
 export { app }
